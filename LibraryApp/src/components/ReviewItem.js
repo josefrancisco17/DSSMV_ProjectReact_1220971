5982dbc0cec788b006f79c2a55c5b8c2d7f64e25
josefrancisco17 <josefranciscoantunesleal@gmail.com>
2023-12-18 22:07:33 +0000

    UI design of Review and ReviewItem

--- LibraryApp/src/components/ReviewItem.js
+++ LibraryApp/src/components/ReviewItem.js
@@ -1,35 +1,74 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
-import {getBook} from "../service/RequestsService";
+import { getBook } from '../service/RequestsService';
 
 const BookItem = ({ review }) => {
-    const [book, setBook] = useState('')
+    const [book, setBook] = useState('');
+
     useEffect(() => {
         const fetchData = async () => {
-            const bookIsbn = review.isbn
-            setBook(await getBook(bookIsbn))
-        }
-        fetchData()
+            const bookIsbn = review.isbn;
+            setBook(await getBook(bookIsbn));
+        };
+        fetchData();
     }, []);
+
+    const formatTimestamp = (timestamp) => {
+        const options = {
+            timeZone: 'Europe/Lisbon'
+        };
+        return new Date(timestamp).toLocaleString('pt-Pt', options);
+    };
+
     return (
-        <View>
-            <View style={styles.bookItem}>
-                <Text>{review.reviewer}</Text>
-                <Text>{book.title}</Text>
-                <Text>{review.review}</Text>
-                <Text>{review.recommended.toString()}</Text>
-                <Text>{review.createdDate}</Text>
-            </View>
+        <View style={styles.bookItem}>
+            <Text style={styles.title}>{review.reviewer}</Text>
+            <Text style={styles.bookTitle}>{book.title}</Text>
+            <Text style={styles.reviewText}>{review.review}</Text>
+            <Text style={[styles.recommendedText, { color: review.recommended ? 'green' : 'darkred', }]}>
+                Recommended: {review.recommended.toString()}
+            </Text>
+            <Text style={styles.dateText}>{formatTimestamp(review.createdDate)}</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     bookItem: {
-        padding: 15,
+        padding: 20,
         marginBottom: 10,
-        backgroundColor: 'blue',
+        backgroundColor: '#2c2c2c',
         borderRadius: 10,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#FFFFFF',
+        marginBottom: 10,
+        textAlign: 'center',
+    },
+    bookTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#FFFFFF',
+        marginBottom: 10,
+        textAlign: 'center',
+    },
+    reviewText: {
+        fontSize: 18,
+        color: '#FFFFFF',
+        marginBottom: 10,
+        textAlign: 'center',
+    },
+    recommendedText: {
+        fontSize: 16,
+        marginBottom: 10,
+        textAlign: 'center',
+    },
+    dateText: {
+        fontSize: 14,
+        color: '#B0BEC5',
+        textAlign: 'center',
     },
 });
 
