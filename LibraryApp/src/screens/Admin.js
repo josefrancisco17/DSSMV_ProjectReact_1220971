7681ed93f27d995c12d2edb17c3014c3c48cb953
josefrancisco17 <josefranciscoantunesleal@gmail.com>
2023-12-18 12:22:20 +0000

    UI design of Admin Menu and React Native version update

--- LibraryApp/src/screens/Admin.js
+++ LibraryApp/src/screens/Admin.js
@@ -23,25 +23,25 @@ const AdminScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
             <TouchableOpacity onPress={logOut} style={styles.logoutButton}>
-                <Text style={styles.buttonText}>Logout</Text>
+                <Text style={styles.text}>Logout</Text>
             </TouchableOpacity>
 
-            <Text style={styles.headerText}>Admin Menu</Text>
+            <Text style={styles.title}>Admin Menu</Text>
 
             <View style={styles.sectionContainer}>
-                <Text style={styles.sectionText}>Manage Libraries</Text>
+                <Text style={styles.text}>Manage Libraries</Text>
                 <TouchableOpacity style={styles.button} onPress={handleCreateLibrary}>
-                    <Text style={styles.buttonText}>Create Library</Text>
+                    <Text style={styles.text}>Create Library</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.button} onPress={handleDeleteLibrary}>
-                    <Text style={styles.buttonText}>Delete Library</Text>
+                    <Text style={styles.text}>Delete Library</Text>
                 </TouchableOpacity>
             </View>
 
             <View style={styles.sectionContainer}>
-                <Text style={styles.sectionText}>Manage Books</Text>
+                <Text style={styles.text}>Manage Books</Text>
                 <TouchableOpacity style={styles.button} onPress={handleCreateBook}>
-                    <Text style={styles.buttonText}>Create Book</Text>
+                    <Text style={styles.text}>Create Book</Text>
                 </TouchableOpacity>
             </View>
         </View>
@@ -52,41 +52,41 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
-        backgroundColor: '#f0f0f0',
-    },
-    logoutButton: {
-        backgroundColor: 'blue',
-        alignSelf: 'flex-end',
-        padding: 10,
-        borderRadius: 8,
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 16,
-        fontWeight: "bold",
-    },
-    headerText: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 20,
-        marginBottom: 10,
-        color: '#333',
+        backgroundColor: '#1a1a1a',
     },
     sectionContainer: {
         marginBottom: 20,
     },
-    sectionText: {
-        fontSize: 18,
+    title: {
+        fontSize: 28,
         fontWeight: 'bold',
-        marginBottom: 10,
-        color: '#555',
+        marginTop: 50,
+        marginBottom: 60,
+        color: '#ccc',
+        alignSelf: 'center',
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: '#ccc',
+        alignSelf: 'center',
     },
     button: {
         backgroundColor: '#007bff',
-        padding: 15,
-        alignItems: "center",
+        paddingTop: 20,
+        margin: 15,
+        alignItems: 'center',
+        justifyContent: 'center',
         borderRadius: 8,
-        marginBottom: 10,
+    },
+    logoutButton: {
+        backgroundColor: '#333',
+        width: 100,
+        alignSelf: 'flex-end',
+        justifyContent: 'center',
+        paddingTop: 15,
+        borderRadius: 8,
     },
 })
 
